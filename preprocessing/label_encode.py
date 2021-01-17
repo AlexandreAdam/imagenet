dfc03d3eabb5e566ae608968971169b34efb32a3
Julia Potokina <julia.potokina@gmail.com>
2021-01-17 18:03:30 -0500

    Added merged df containing labels and their names

--- preprocessing/label_encode.py
+++ preprocessing/label_encode.py
@@ -14,21 +14,20 @@ import pandas as pd
 import os
 from pprint import pprint
 
+
+
 def main(datapath):
     train_path = os.path.join(datapath, "train")
     val_path = os.path.join(datapath, "val")
     df = pd.read_csv(os.path.join(datapath, "words.txt"), sep="\t", names=["wnind", "synset"])
     df_tiny = pd.read_csv(os.path.join(datapath, "wnids.txt"), names=["wnind"])
-    pprint(df_tiny)
-    df_tiny["synset"] = df[df["wnind"].isin(df_tiny["wnind"])]
-    pprint(df_tiny.head())
-
-
+    df_merged_label = pd.merge(df,df_tiny,on='wnind')
+    df_merged_label.insert(0, "id", df_merged_label.index)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("-d", "--datapath", type=str, default="../../data/tiny-imagenet-200")
+    parser.add_argument("-d", "--datapath", type=str, default="/media/julia/Seagate/Data/tiny-imagenet-200")
     args = parser.parse_args()
-    
+
     main(args.datapath)
